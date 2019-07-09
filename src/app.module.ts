92ad72778612253bcc53798a317ed0c78c94b384
luqezman <joao.galiano.silva@gmail.com>
2019-07-09 23:57:03 +0100

    feat(mongoose): Add nest mongoose packages

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,19 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { MongooseModule } from '@nestjs/mongoose';
 
-import { GqlConfigService } from './services/gql-config.service';
 import { JobResolver } from './resolvers/job/job.resolver';
+import { GqlConfigService } from './services/gql-config.service';
 import { JobService } from './services/job.service';
+import { MongooseConfigService } from './services/mongoose-config.service';
 
 @Module({
   imports: [
     GraphQLModule.forRootAsync({
       useClass: GqlConfigService,
+    }),
+    MongooseModule.forRootAsync({
+      useClass: MongooseConfigService,
     }),
   ],
   providers: [
---
+++ src/services/mongoose-config.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { MongooseModuleOptions, MongooseOptionsFactory } from '@nestjs/mongoose';
+
+@Injectable()
+export class MongooseConfigService implements MongooseOptionsFactory {
+  createMongooseOptions(): MongooseModuleOptions {
+    return {
+      uri: 'mongodb://localhost/nest',
+    };
+  }
+}
